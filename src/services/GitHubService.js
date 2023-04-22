22261aa51b8c73c8a770f1fae0fef1815f0bdbc5
Alexandr Shtyb <styb701@mail.ru>
2023-04-22 16:05:40 +0300

    changed service

--- src/services/GitHubService.js
+++ src/services/GitHubService.js
@@ -1,12 +1,11 @@
 const GitHubServices = () => {
   const getAllCards = async (subject) => {
     const res = await fetch(`https://api.github.com/search/repositories?q=${subject}`);
-    const resp = await res.json();
-    return resp.items.map(_transformCards);
+    const json = await res.json();
+    return json.items.map(_transformCards);
   }
 
   const _transformCards = (cards) => {
-    console.log('cardsss', typeof cards);
     return {
       id: cards.id,
       nameProject: cards.full_name,
